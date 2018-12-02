1d006eb30f481d7b1d26614ed9b0004072f3f3ab
Luckercorgi240 <40528544+Luckercorgi240@users.noreply.github.com>
2018-12-01 16:21:48 -0800

    New background(Again)
    I made the background way much better

--- chapter16/stickman/stickmangame.py
+++ chapter16/stickman/stickmangame.py
@@ -15,6 +15,7 @@ class Game:
         self.canvas_height = 500
         self.canvas_width = 500
         self.bg = PhotoImage(file="background.gif")
+        self.bg2 = PhotoImage(file="background2.gif")
         w = self.bg.width()
         h = self.bg.height()
         pic = False
@@ -24,6 +25,7 @@ class Game:
                     self.canvas.create_image(x * w, y * h, image=self.bg, anchor='nw')
                     pic = False
                 else:
+                    self.canvas.create_image(x * w, y * h, image=self.bg2, anchor='nw')
                     pic = True
         self.sprites = []
         self.running = True

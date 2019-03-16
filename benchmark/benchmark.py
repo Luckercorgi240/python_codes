4d94444774d30dc8fc0097720d23a38887af6932
Pan Zhang <panzhang0216@gmail.com>
2019-03-16 15:00:20 -0700

    Benchmark
    This is a benchmarking code for my raspberry pi's

---
+++ benchmark/benchmark.py
@@ -0,0 +1,25 @@
+import time
+
+def duration():
+    start = time.time()
+    for x in range(1000000):
+        pass
+    end = time.time()
+    d = end - start
+    print(d)
+    return d
+
+
+def average(list):
+	number = sum(list)
+	anwser = number / len(list)
+	print('This is the average      %f' % anwser)
+
+i = 0
+z = []
+while i < 100:
+    d = duration()
+    z.append(d)
+    i = i + 1
+
+average(z)
